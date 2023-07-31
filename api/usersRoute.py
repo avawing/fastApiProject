d0edfb2077bdd20d4616b837ebcce4ddaac9b9e6
Ava Wingfield <avelinewingfield@gmail.com>
2023-07-31 12:36:21 -0400

    Load Test

--- api/usersRoute.py
+++ api/usersRoute.py
@@ -30,11 +30,8 @@ async def read_users(db: Session = Depends(database.get_db)) -> List[User]:
 
 @router.post("/")
 async def create_user(user: User, db: Session = Depends(database.get_db)) -> User:
-    db_user = User(first_name=user.first_name,
-                   last_name=user.last_name,
-                   email=user.email,
-                   has_loan=user.has_loan,
-                   has_other_load=user.has_other_loan)
+    db_user = usersModel.User(first_name=user.first_name, last_name=user.last_name, email=user.email,
+                              has_loan=user.has_loan, has_other_loan=user.has_other_loan)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
@@ -45,19 +42,15 @@ async def create_user(user: User, db: Session = Depends(database.get_db)) -> Use
 
 @router.get("/{user_id}")
 async def read_user(user_id: int, db: Session = Depends(database.get_db)) -> User:
-    db_user = db.query(usersModel.User).filter(User.id == user_id).first()
+    db_user = db.query(usersModel.User).filter(usersModel.User.id == user_id).first()
     return db_user
 
 
 @router.put("/{user_id}")
 async def update_user(user_id: int, user: User, db: Session = Depends(database.get_db)) -> User:
-    db_user = await db.query(User).filter(User.id == user_id).first()
+    db_user = db.query(usersModel.User).filter(usersModel.User.id == user_id).first()
 
-    for k, v in db_user:
-        if db_user[k] != user[k]:
-            db_user[k] = user[k]
-
-    db .add(db_user)
+    db_user.has_loan = user.has_loan
     db.commit()
     db.refresh(db_user)
 
@@ -66,7 +59,7 @@ async def update_user(user_id: int, user: User, db: Session = Depends(database.g
 
 @router.delete("/{user_id}")
 async def delete_user(user_id: int, db: Session = Depends(database.get_db)) -> bool:
-    user = await db.query(User).filter(User.id == user_id).first()
+    user = db.query(usersModel.User).filter(usersModel.User.id == user_id).first()
     db.delete(user)
     db.commit()
     return True
--- tests/users.py
+++ tests/users.py
@@ -16,27 +16,30 @@ def test_get_users():
     assert response.status_code == 200
 
 
+#fail
 def test_get_user():
     response = client.get('/users/1')
     assert response.status_code == 200
 
 
+#fail
 def test_delete_user():
     response = client.delete('/users/1')
     assert response.status_code == 200
 
 
 def test_create_user():
-    data = {"first_name": "Fred", "last_name": "fredderton", "email": "fred@fred.com", "has_loan": True,
+    data = {"first_name": "Fred", "last_name": "fredderton", "email": "fred@george.com", "has_loan": True,
             "has_other_loan": False}
 
-    response = client.post('/users/', json=json.dumps(data))
+    response = client.post('/users/', json=data)
     assert response.status_code == 200
 
 
 def test_update_user():
-    data = {"first_name": "Robert", "last_name": "Robertington"}
-    response = client.put('/users/1', json=json.dumps(data))
+    data = {"first_name": "Fred", "last_name": "fredderton", "email": "bobtastic@bob.com", "has_loan": True,
+            "has_other_loan": False}
+    response = client.put('/users/2', json=data)
     assert response.status_code == 200
 
 
